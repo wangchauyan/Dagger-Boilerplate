e4e3919cc7ac0741a9b701d9b9ac5bc7e7672b39
chauyan.wang <wang.chauyan@gmail.com>
2020-02-10 15:34:17 -0800

    [DEV-0] Remove subcomponent dependency in Resource Module

--- app/src/main/java/idv/chauyan/dagger_boilerplate/di/ResourceModule.kt
+++ app/src/main/java/idv/chauyan/dagger_boilerplate/di/ResourceModule.kt
@@ -5,10 +5,7 @@ import dagger.Provides
 import idv.chauyan.dagger_boilerplate.model.Car
 import idv.chauyan.dagger_boilerplate.ui.main.MainActivityComponent
 
-@Module (subcomponents = [
-        MainActivityComponent::class
-    ]
-)
+@Module
 class ResourceModule {
     @Provides
     fun provideCar(): Car = Car()
